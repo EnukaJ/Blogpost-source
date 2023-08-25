3249f42fe2831d79393ffc377656fc9baccffeae
EnukaJ <enukaj8@gmail.com>
2023-08-25 12:39:56 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="dark:bg-slate-800">
+      <body className="dark:bg-slate-800 h-full">
         <Navbar />          
         <main className='px-4 md:px-6 prose prose-xl prose-slate dark:prose-invert mx-auto'>
           {children}
